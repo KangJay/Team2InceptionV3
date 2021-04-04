30005ce2652379d312a3118f6f9f5a0ed218f73f
Ji-Hoon Kang <jkang18@uw.edu>
2021-04-04 00:23:36 -0700

    binary-files has two numpy files. One for image paths, one for labels. shuffled already. Still need to do bath

---
+++ binary-files/test.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+images = np.load('./images.npy')
+labels = np.load('./labels.npy')
+print(images)
+print(labels)
--- densenet.py
+++ densenet.py
@@ -27,6 +27,9 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator,img_to_array
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
+from tensorflow import keras
+from tensorflow.keras import layers
+from numpy import save
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -46,7 +49,7 @@ x= Dense(512,activation='relu')(x)
 x= BatchNormalization()(x)
 x= Dropout(0.5)(x)
 
-preds=Dense(8,activation='softmax')(x) #FC-layer
+preds=Dense(200,activation='softmax')(x) #FC-layer
 model=Model(inputs=model_d.input,outputs=preds)
 
 # Freeze layers except last 8
@@ -75,17 +78,32 @@ def iterate_dirs(path, subdir):
 """
 # Helper method for recursive 
 def iterate_dirs():
+    image_paths, labels = np.empty(0), np.empty(0) 
     image_labels = os.listdir(os.getcwd() + "/tiny-imagenet-200/train")
     random.shuffle(image_labels) # shuffle to avoid possible bias in structure alone
+    num_class = 0
     for img in image_labels:
         images = sorted(os.listdir(subpath + img + "/images/"))
-        #print(images)
+        #print(len(images))
         for image in images:
-            image = cv2.imread(f"{subpath}/{img}/images/{image}")
+            image = f"{subpath}{img}/images/{image}"
+            image_paths = np.append(image_paths, image)
+            labels = np.append(labels, img)
+            #print(labels)
+            """for image in images:
+            image = cv2.imread(f"{subpath}{img}/images/{image}")
             image = cv2.resize(image, (128, 128))
             image = img_to_array(image)
             data.append(image)
-            labels.append(img)
+            labels.append(img)"""
+        print(f"Done with {img}. File {num_class}/200")
+        num_class += 1
+    #print(image_paths.shape)
+    #print(labels.shape)
+    from sklearn.utils import shuffle
+    images_shuffled, labels_shuffled = shuffle(image_paths, labels)
+    save('./binary-files/images.npy', images_shuffled)
+    save('./binary-files/labels.npy', labels_shuffled)
 
 if __name__ == "__main__":
     iterate_dirs()
